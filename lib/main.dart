4fcdb5c21c586eb4ea046aab42b01bd73d685acc
lbarsidati22 <lbarsidati575@gmail.com>
2024-11-02 02:11:51 +0000

    add bloc observice class

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:news_app/news_app/news_layout.dart';
+import 'package:news_app/shared/bloc_observer.dart';
 
 void main() {
+  Bloc.observer = MyBlocObserver();
   runApp(const MyApp());
 }
 
